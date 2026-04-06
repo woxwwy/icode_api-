204e655375de88aecb4abb28dd3955ce2570e023
jinxiaoji <1330045093@qq.com>
2026-04-06 21:07:21 +0800

    Enhance ScgGatewayApplication with component scanning
    Added component scanning for 'com.gateway' package and enabled MBean export with existing registration policy.

--- gateway-service/scg-project/src/main/java/com/example/scg_project/ScgGatewayApplication.java
+++ gateway-service/scg-project/src/main/java/com/example/scg_project/ScgGatewayApplication.java
@@ -2,8 +2,15 @@ package com.example.scg_project;
 
 import org.springframework.boot.SpringApplication;//启动spring程序的工具
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jmx.support.RegistrationPolicy;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.scg_project", "com.gateway"}) // 这行告诉Spring去扫描com.gateway包，这样我们写的RateLimitFilter才能被Spring发现并注册到容器中
+
+@org.springframework.context.annotation.EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+
 public class ScgGatewayApplication {
 
     //java程序的入口，main方法是程序的起点，运行启动整个spring应用
